a56a4cf580b69b2893e990eda596ab66b2476ab1
Sukumar <192.surya@gmail.com>
2025-05-09 11:57:14 +0530

    get user api route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -13,6 +15,8 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
 const app = express();
 
+app.use(cookieParser());
+
 app.use(express.json());
 
 app.listen(3000, () => {
@@ -20,6 +24,8 @@ app.listen(3000, () => {
 });
 
 app.use('/api/auth', authRoutes);
+
+app.use('/api/user', userRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
