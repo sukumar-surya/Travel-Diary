1c59146204194ccc5ddade57c4f42b17f5287d3e
Sukumar <192.surya@gmail.com>
2025-06-26 20:04:17 +0530

    Navbar

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -2,3 +2,16 @@ export const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 };
+
+export const getInitials = (name) => {
+    if (!name) return '';
+
+    const words = name.split(' ');
+
+    let initials = '';
+
+    for (let i = 0; i < Math.min(words.length, 2); i++) {
+        initials += words[i].charAt(0).toUpperCase();
+    }
+    return initials;
+}
