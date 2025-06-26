deea112ddfa69ce7aa3711abdb853589759fbfa3
Sukumar <192.surya@gmail.com>
2025-06-26 12:34:38 +0530

    loading btn rectification

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -76,7 +76,7 @@ const Login = () => {
             {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
 
             {loading ? (
-              <span className='animate-pulse'>Loading...</span>          
+              <p className='animate-pulse w-full text-center btn-primary'>LOADING...</p>          
             ) : (<button type='submit' className='btn-primary'>LOGIN</button>)}
 
             <p className='text-xs text-slate-500 text-center my-4'>Or</p>
