15ab8b73a2fc0417c7c5cad6217e4d52b663e1af
Sukumar <192.surya@gmail.com>
2025-07-03 18:00:55 +0530

    travel story card component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import axiosInstance from '../../utils/axiosinstance'
+import TravelStoryCard from '../../components/TravelStoryCard'
 
 const Home = () => {
   const [allStories, setAllStories] = React.useState([])
@@ -17,6 +18,12 @@ const Home = () => {
     }
   }
 
+  const handleEdit = async (data) => {}
+
+  const handleViewStory = async (data) => {}
+
+  const updateIsFavorite = async (data) => {}
+
   React.useEffect(() => {
     getAllTravelStories()
     return () => {}
@@ -27,8 +34,31 @@ const Home = () => {
 
     <div className='container mx-auto py-10'>
       <div className='flex gap-7'>
-        <div className='flex-1'></div>
-
+        <div className='flex-1'>
+          {allStories.length > 0 ? (
+            <div className='grid grid-cols-2 gap-4'>
+              {allStories.map((item) => {
+                return (
+                  <TravelStoryCard
+                    key={item._id} 
+                    imageUrl={item.imageUrl}
+                    title={item.title}
+                    story={item.story}
+                    date={item.visitDate}
+                    visitedLocation={item.visitedLocation}
+                    isFavorite={item.isFavorite}
+                    onEdit={() => handleEdit(item)}
+                    onClick={() => handleViewStory(item)}
+                    onFavoriteClick={() => updateIsFavorite(item)}
+                  />
+                )
+              })}
+            </div>
+          ) : (
+            <div>Empty Card Here</div>
+          )}
+        </div>
+            
         <div className='w-[320px]'></div>
       </div>
     </div>
