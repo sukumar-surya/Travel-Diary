4dd43190b4a11f9b083481c206bbcfd212e7202a
Sukumar <192.surya@gmail.com>
2025-08-19 13:47:52 +0530

    base_url update

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.BACKEND_URL || "http://localhost:3000/api";
+const BASE_URL = import.meta.env.BACKEND_URL || "http://localhost:3000/api";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
